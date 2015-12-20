ccc3e16574bb92144e32448cbed0e439f8ded745
miaoshua <945970809@qq.com>
2015-12-20 15:36:30 +0800

    server类的实现

---
+++ server.h
@@ -0,0 +1,44 @@
+/*======================================================
+    > File Name: server.h
+    > Author: MiaoShuai
+    > E-mail:  
+    > Other :  
+    > Created Time: 2015年12月20日 星期日 15时07分50秒
+ =======================================================*/
+#ifndef SERVER_H_
+#define SERVER_H_
+
+#include "base_loop.h"
+#include <memory>
+#include <string>
+#include "callback.h"
+
+namespace netlib
+{
+    class Server
+    {
+        public:
+            Server(std::string ip,int port,int loopNumber);
+            ~Server();
+            void start(void);   //开始运行server
+            void setConnectionCallback(connectionCallback cb)
+            {
+                connectionCallback_ = cb;
+            }
+            void setReadCallback(readCallback cb)
+            {
+                readCallback_ = cb;
+            }
+            void setCloseCallback(closeCallback cb)
+            {
+                closeCallback_ = cb;
+            }
+        private:
+            std::shared_ptr<BaseLoop> baseLoopPtr_; //保存baseLoop的智能指针
+            connectionCallback connectionCallback_;
+            readCallback readCallback_;
+            closeCallback closeCallback_;
+    };
+}
+
+#endif
