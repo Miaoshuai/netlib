3ab5510141f46d0b1b0093dcaec29ef959fc0dbe
miaoshua <945970809@qq.com>
2015-12-20 14:59:57 +0800

    主loop循环类

--- base_loop.h
+++ base_loop.h
@@ -10,14 +10,25 @@
 
 #include <map>
 #include "callback.h"
+#include "event_loop_thread_pool.h"
+#include <memory>
+#include <string>
 
 namespace netlib
 {
     class BaseLoop
     {
         public:
+            BaseLoop(std::string ip,int porti,int loopNumber);  //构造传参确定开多少个loop
+            ~BaseLoop();
+            int createListenFd(void);  //创建监听套接字
+            void startListen(void);      //开始监听
+            void connectionHandle();    //连接回调可以在此之中调用
         private:
-            connectionCallback connectionCallback_;     //新连接的回调函数
+            std::string ip_;        //监听ip
+            int         port_;      //监听端口
+            int         listenFd;   //监听套接字
+            std::shared_ptr<LoopThreadPool> loopThreadPoolPtr_  //线程池对象指针          
     };
 }
 
