215b3300948705149269f085cb1b00b6db7f3663
miaoshua <945970809@qq.com>
2015-12-20 16:51:59 +0800

    事件循环的基本封装

--- event_loop.h
+++ event_loop.h
@@ -9,7 +9,7 @@
 #ifndef EVENTLOOP_H_
 #define EVENTLOOP_H_
 
-#include <memory.h>
+#include <memory>
 #include <sys/epoll.h>
 #include "epoll.h"
 #include "callback.h"
@@ -19,23 +19,23 @@ namespace netlib    //自定义命名空间netlib
     class EventLoop
     {
         public:
-            EventLoop();
+            EventLoop(int eventFd);
             ~EventLoop();
             void loop();
             void handleEventFdRead(void);
-            void setReadCallback(readCallback &cb)
+            void setReadCallback(readCallback cb)
             {
                 readCallback_ = cb;
             }
-            void setCloseCallback(closeCallback &cb)
+            void setCloseCallback(closeCallback cb)
             {
                 closeCallback_ = cb;
             }
 
         private:
-         eventFd_;  //用于主loop向其他loop传递新连接的文件描述符
-         shared_ptr<Epoll>;  //所拥有的epoll循环
-         struct event* events_;  //获取epoll的就绪事件
+         int eventFd_;  //用于主loop向其他loop传递新连接的文件描述符
+         std::shared_ptr<Epoll> epoll_;  //所拥有的epoll循环
+         struct epoll_event events_[1024];  //获取epoll的就绪事件
          readCallback readCallback_;    //读回调
          closeCallback closeCallback_;  //关闭回调
     };
