2adb635b14a98f88057643d623e339e71ede6d95
miaoshua <945970809@qq.com>
2015-12-21 13:59:27 +0800

    loop循环线程池

--- event_loop_thread_pool.h
+++ event_loop_thread_pool.h
@@ -13,26 +13,43 @@
 #include <functional>
 #include <thread>
 #include <memory>
+#include "callback.h"
 
 namespace netlib
 {
     class LoopThreadPool
     {
         public:
-            typedef std::function<void(void)> threadFunc;
             LoopThreadPool(int number);
             ~LoopThreadPool();
             int getNextLoop();             //返回下一个loop的eventfd
             void start();                   //开始运行loop线程池
             int createEventFd();            //创建eventfd
-            void threadFunc();              //线程函数，用于创建event_loop
-            void fun(void);                 //暂时用于测试的处理读回调的函数
+            static void threadFunc(int eventFd,LoopThreadPool *loopThreadPool);              //线程函数，用于创建event_loop
+            static void fun(void);                 //暂时用于测试的处理读回调的函数
+            void setReadCallback(readCallback cb)
+            {
+                readCallback_ = cb;
+            }
+            void setCloseCallback(closeCallback cb)
+            {
+                closeCallback_ = cb;
+            }
+            readCallback getReadCallback(void)
+            {
+                return readCallback_;   
+            }
+            closeCallback getCloseCallback(void)
+            {
+                return closeCallback_;
+            }
         private:
             int loopNumber_;    //loop_thread的总数
             int next_;          //下一个要执行任务的loop
             std::map<int,int> fdMap_; //用于保存整数与每个loop的eventfd的映射
-            std::vector<std::shared_ptr<thread>> threadVector_; //保存新创建线程的对象的智能指针
-
+            std::vector<std::shared_ptr<std::thread>> threadVector_; //保存新创建线程的对象的智能指针
+            readCallback readCallback_;         //读回调
+            closeCallback closeCallback_;       //关闭回调
     };   
 }
 
